5568ead1a682885e6b424f644d4bccbc40e541de
DuongThinh46 <thinhdnse171879@fpt.edu.vn>
2025-05-22 15:11:17 +0700

    CRUD Brand

---
+++ Application/Features/Brands/Queries/GetAllBrand/GetAllBrandsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetAllBrand
+{
+    public class GetAllBrandsViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+        public long CreatedAt { get; set; }
+        public long UpdatedAt { get; set; }
+    }
+}
